43b7b6e6f26d15c22a4e5d77fc88a7b326940fe3
quickblack1 <najmeedaud@gmail.com>
2025-12-30 15:19:45 +0800

    a

--- gd/vehicle_body_3d.gd
+++ gd/vehicle_body_3d.gd
@@ -1,7 +1,9 @@
 extends VehicleBody3D
 
 @export var MAX_STEER = 0.5
-@export var ENGINE_POWER = 300
+@export var ENGINE_POWER = 500
+
+@onready var speed_label = $"SpeedLabel"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +17,8 @@ func _process(_delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
 	engine_force = Input.get_axis("ui_up", "ui_down") * ENGINE_POWER
+	
+	#show car speed
+	var forward_speed = -transform.basis.z.dot(linear_velocity)
+	var speed_kmh = forward_speed * 3.6
+	speed_label.text = "Speed: %d km/h" % abs(speed_kmh)
